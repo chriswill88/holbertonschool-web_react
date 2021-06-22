548c02432add8a9fb5f24f2d0d1e5f64d3baa976
chriswill88 <williechri79@gmail.com>
2021-06-22 18:20:22 -0400

    task 0 and setup

---
+++ 0x00-TypeScript/task_0/js/main.js
@@ -0,0 +1,25 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+
+const student1: Student = {
+    firstName: 'bill',
+    lastName: 'joel',
+    age: 24,
+    location: 'Netherlands',
+}
+
+const student2: Student = {
+    firstName: 'Jeff',
+    lastName: 'joel',
+    age: 24,
+    location: 'Netherlands',
+}
+
+const studentList = [student1, student2]
+for (let i of studentList) {
+    console.log(i);
+}
