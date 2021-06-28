40289467e5b4b8715a3c50c9f39561d0c6cd94cd
chriswill88 <williechri79@gmail.com>
2021-06-28 13:18:00 -0400

    completed task 0: added the table

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -18,8 +18,18 @@ const student2: Student = {
     age: 24,
     location: 'Netherlands',
 }
-
 const studentList: Array<Student> = [student1, student2];
+
+const table = document.createElement('table');
+document.body.appendChild(table);
+
 for (let i of studentList) {
-    console.log(i);
+    const tr = document.createElement('tr');
+    table.appendChild(tr);
+
+    for (let x of [i.firstName, i.location]) {
+        let tdElement = document.createElement('td');
+        tdElement.innerHTML = x;
+        tr.appendChild(tdElement);
+    }
 }
