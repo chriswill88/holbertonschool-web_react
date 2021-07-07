24830e051636649382170e12fbdf1a9e510c74b2
chriswill88 <williechri79@gmail.com>
2021-07-07 17:05:27 -0400

    Task 2: done

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,16 @@
+var $ = require("jquery");
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append("<p>Copyright - Holberton School</p>");
+
+let count = 0;
+function updateCounter() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+}
+
+$('#count').text(`${count} clicks on the button`);
+const fun = _.debounce(updateCounter, 500);
+$(':button').on('click', fun);
---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/dashboard_main.js',
+    output: {
+      path: path.resolve(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: 'url-loader',
+          options: {
+            // Images larger than 10 KB won’t be inlined
+            limit: 10 * 1024
+          }
+        },
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          loader: 'image-webpack-loader',
+          // Specify enforce: 'pre' to apply the loader
+          // before url-loader/svg-url-loader
+          // and not duplicate it in rules with them
+          enforce: 'pre'
+        }
+      ],
+    },
+  };
