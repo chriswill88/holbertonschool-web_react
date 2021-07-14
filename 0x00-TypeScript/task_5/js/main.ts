5c9b4161b2ff56630eea16068fc0b96c384a9d4b
chriswill88 <williechri79@gmail.com>
2021-07-14 13:43:31 -0400

    Done all

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -12,7 +12,6 @@ export function sumMajorCredits(subject1: MajorCredits, subject2: MajorCredits):
   return {credits: subject1.credits + subject2.credits} as MajorCredits;
 }
 
-
 export function sumMinorCredits(subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
   return {credits: subject1.credits + subject2.credits} as MinorCredits;
 }
