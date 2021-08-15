bb4f947514124ffd3e341eeaae56196b58895b3e
chriswill88 <williechri79@gmail.com>
2021-08-15 17:16:00 -0400

    task_3

--- 0x03-react_props/task_0/dashboard/src/App/App.test.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.test.js
@@ -3,6 +3,9 @@ import {expect, jest, test} from '@jest/globals';
 import { shallow, configure} from 'enzyme';
 import App from './App'
 import Notifications from '../Notifications/Notifications'
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 configure({ adapter: new Adapter() });
@@ -45,5 +48,33 @@ describe('<Notifications>', () => {
   it ('renders app', () => {
     const wrapper = shallow(<Notifications />)
     expect(wrapper.contains('Here is the list of notifications')).toBe(true)
+  })
+})
+
+describe('<Login>', () => {
+  it ('renders app', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(Notifications)).toHaveLength(1)
+  })
+})
+
+describe('<Login>', () => {
+  it ('renders app', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(Header)).toHaveLength(1)
+  })
+})
+
+describe('<Login>', () => {
+  it ('renders app', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(Login)).toHaveLength(1)
+  })
+})
+
+describe('<Login>', () => {
+  it ('renders app', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(Footer)).toHaveLength(1)
   })
 })
