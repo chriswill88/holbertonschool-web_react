9cd872203ed9428dc22d7a749e0d8a53b04a34ed
chriswill88 <williechri79@gmail.com>
2021-09-01 05:32:44 -0400

    [ADD]

--- 0x03-react_props/task_0/dashboard/src/App/App.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -7,22 +7,26 @@ import Header from '../Header/Header'
 import { getFullYear, getFooterCopy } from '../utils/utils';
 
 
-function App({ logo, x }) {
-  return (
-    <>
-      <Notifications x={x}/>
-      <div className="App" >
-        <Header logo={logo}/>
-        <hr />
-        <body className = 'App-body' >
-          <Login />
-        </body>
-        <footer className = 'App-footer'>
-          <Footer getFullYear={getFullYear} getFooterCopy={getFooterCopy}/>
-        </footer>
-      </div>
-    </>
-  );
+class App extends React.Component {
+  render() {
+    const { logo, x } = this.props;
+
+    return (
+      <>
+        <Notifications x={x}/>
+        <div className="App" >
+          <Header logo={logo}/>
+          <hr />
+          <body className = 'App-body' >
+            <Login />
+          </body>
+          <footer className = 'App-footer'>
+            <Footer getFullYear={getFullYear} getFooterCopy={getFooterCopy}/>
+          </footer>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
